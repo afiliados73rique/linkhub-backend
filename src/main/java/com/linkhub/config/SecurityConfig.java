ed1cd85839e14b5cb656cea6ae630e0943071da0
millenaameire <vitoriamillena453@gmail.com>
2026-07-25 23:29:49 -0300

    urls adicionadas a SecurityConfig

--- src/main/java/com/linkhub/config/SecurityConfig.java
+++ src/main/java/com/linkhub/config/SecurityConfig.java
@@ -25,7 +25,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://*.vercel.app/"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "https://*.vercel.app",
+                "https://linkhub-afiliado.com.br",
+                "https://www.linkhub-afiliado.com.br"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
